b2dd08e79b9245b8e4c7ef4e596219e4f0476444
William Schaub <william.schaub@genesi-usa.com>
2012-04-16 16:47:55 -0400

    installheadless.sh: generate images using the loop device. installheadless.sh can now generate sd card images if you pass it a loop device and a blank file of the desired size. this only works if you modprobe the loop device with the max_part parameter set to     nonzero. just modprobe loop max_part=63 or boot the kernel with     loop.max_part=63 if it is not a module.

--- installheadless.sh
+++ installheadless.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DEVICE=$1
+LOOP=0
 if [ -z $DEVICE ] || [ ! -b $DEVICE ]; then
     echo "$DEVICE is not a valid device! Exiting"
     exit 1
@@ -12,6 +13,20 @@ elif [[ $DEVICE =~ \/dev\/sd* ]]; then
     echo "Generic SCSI block device, partitions named sd*N..."
     BOOTPART=${DEVICE}1
     ROOTPART=${DEVICE}2
+elif [[ $DEVICE =~ \/dev\/loop* ]]; then
+    BOOTPART=${DEVICE}p1
+    ROOTPART=${DEVICE}p2
+    FILE=$2
+    if [ -f $FILE ]; then
+        LOOP=1
+        echo loop device detected
+    else
+        echo "you have to give an image file name when using a loop device"
+        echo "you also want to be sure that you did not run losetup yourself"
+        echo "we will do that for you"
+        exit 1
+    fi
+
 else
     echo "unknown type of device!"
     exit 1
@@ -52,7 +67,10 @@ if [ $WILL_FORMAT != "yes" ]; then
 fi 
 #make extra sure we bail out if anything goes wrong. 
 set -e
-
+if [ $LOOP -eq 1 ]; then
+    echo "attaching $FILE to $DEVICE"
+    losetup $DEVICE $FILE
+fi
 echo -n "creating MSDOS label on the device..."
 parted $DEVICE --script -- mklabel msdos
 echo "done"
@@ -65,6 +83,14 @@ echo "done"
 echo -n "creating root partition..."
 parted $DEVICE --align optimal --script -- mkpart primary 128 -1
 echo "done"
+if [ $LOOP -eq 1 ]; then
+    echo "detaching $FILE from $DEVICE"
+    losetup -d $DEVICE
+    sleep 3
+    echo "attaching $FILE to $DEVICE"
+    losetup $DEVICE $FILE
+    sleep 5
+fi
 
 echo -n "preparing boot partition in $BOOTPART..."
 mkfs.ext2 -L $BOOTNAME -q $BOOTPART
@@ -95,4 +121,8 @@ if [ -d $TARGETROOT/home/oem ]; then
 fi
 umount $TARGETBOOT
 umount $TARGETROOT
+if [ $LOOP -eq 1 ]; then
+    echo "detaching $FILE from $DEVICE"
+    losetup -d $DEVICE
+fi
 echo "All done, you are ready to put the SD card in your Efika and boot it."
